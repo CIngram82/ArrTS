58a7ae0fbcf6aad15ce47653a70a34489353365e
CIngram82 <cingram82@gmail.com>
2018-07-18 21:04:39 -0400

    Network Manager
    Network Manager added. Temp player prefab added for testing

---
+++ ArrTS/Assets/MyHud.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyHud : MonoBehaviour {
+    private NetworkManager networkManager;
+	// Use this for initialization
+	void Start () {
+        networkManager = GetComponent<NetworkManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+     public void MyStartHost()
+    {
+        networkManager.StartHost();
+
+        Debug.Log("Starting Houst @ : " + Time.realtimeSinceStartup);
+    }
+
+    void OnStartHost()
+    {
+        Debug.Log("Host Started @ : " + Time.realtimeSinceStartup);
+    }
+}
