57aaac8cc152dbdceba4cb2e6b80c0c2d2e18cd2
CIngram82 <cingram82@gmail.com>
2018-07-30 11:58:12 -0400

    Logging Host contections
    Setting up the Log entrys for conecting to the host server

---
+++ ArrTS/Assets/MyNetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+    public void MyStartHost()
+    {
+        StartHost();
+
+        Debug.Log(Time.realtimeSinceStartup + " : Starting Host" );
+        
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log(Time.realtimeSinceStartup + " : Host Started");
+    }
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        base.OnStartClient(myClient);
+        Debug.Log(Time.realtimeSinceStartup + " : Start Client Request");
+    }
+
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+        Debug.Log(Time.realtimeSinceStartup + " : Client Connected to :" + conn.address);
+    }
+}
